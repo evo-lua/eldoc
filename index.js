7407ef0dd37ba33ad29aad97da993e15fd22555b
RDW <duckwhale@mail.ru>
2021-11-06 00:39:20 +0100

    experimental stuff

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
+const CLI = {
+	ParseFile(filePath) {},
+	ParseDirectory(filePath) {}
+}
+
+const GLOBAL_LUA_ENVIRONMENT = "_G"
+
+class DocComment {
+	constructor(text = "", namespace = GLOBAL_LUA_ENVIRONMENT) {
+	}
+}
+
+class FunctionDefinition {
+	constructor(astNode) {
+		this.comments = [];
+		console.log(astNode);
+	}
+}
+
+const OutputFormatter = {
+	DumpAsJSON(docComments) {}
+}
+
+const LuaParser = {
+	OnRelevantTagEncountered(tagName) {},
+	OnCommentEncountered(rawText) {}
+}
+
+
 const AST_NODE_TYPE_COMMENT = "Comment";
 const AST_NODE_TYPE_FUNCTION = "FunctionDeclaration";
 
@@ -9,25 +38,39 @@ let fileContents = fs.readFileSync("C_FileSystem.lua").toString();
 // console.log(fileContents.toString());
 
 
-function OnRelevantTagEncountered(tagName) {}
-
-function OnCommentEncountered(rawText) {}
-
-
-
 let lastCommentNodes = [];
+
+function onFunctionDefinition(astNode) {
+	console.log("NYI: Function def begins here");
+
+	if(astNode.type === AST_NODE_TYPE_FUNCTION) { // OnFunctionDefinitionEncountered
+		console.log("FUNCTION DEF BEGINS: %s (base: %s)", astNode.identifier.identifier.name, astNode.identifier.base.name);
+
+		// if (namespaceOrModuleName === astNode.identifier.base.name)
+		astNode.comments = lastCommentNodes; lastCommentNodes = []; // Reset for next funcdef
+
+		// Relevant properties: Name, arguments, return values, namespace, isLocal =scope
+		astNode.name = astNode.identifier.identifier.name;
+		// astNode.name = astNode.identifier.identifier.name;
+		// params, return values aren't yet available and have to be added AFTER the function def is parsed fully
+
+		exportedApiSurface[astNode.identifier.base.name] = exportedApiSurface[astNode.identifier.base.name] || {};
+		exportedApiSurface[astNode.identifier.base.name][astNode.name] = new FunctionDefinition(astNode);
+
+	}
+}
 
 function onNodeCreated(astNode) {
 	// console.log("Node created\t%s\t\t\t%s", astNode.type, astNode.name || "");
 
 	if(astNode.type === AST_NODE_TYPE_FUNCTION) { // OnFunctionDefinitionEncountered
-		console.log("FUNCTION DEF: %s (base: %s)", astNode.identifier.identifier.name, astNode.identifier.base.name);
+		console.log("FUNCTION DEF ENDS: %s (base: %s)", astNode.identifier.identifier.name, astNode.identifier.base.name);
 
-		// if (namespaceOrModuleName === astNode.identifier.base.name)
-		astNode.comments = lastCommentNodes; lastCommentNodes = []; // Reset for next funcdef
+		// // if (namespaceOrModuleName === astNode.identifier.base.name)
+		// astNode.comments = lastCommentNodes; lastCommentNodes = []; // Reset for next funcdef
 
-		exportedApiSurface[astNode.identifier.base.name] = exportedApiSurface[astNode.identifier.base.name] || [];
-		exportedApiSurface[astNode.identifier.base.name].push(astNode);
+		// exportedApiSurface[astNode.identifier.base.name] = exportedApiSurface[astNode.identifier.base.name] || [];
+		// exportedApiSurface[astNode.identifier.base.name].push(astNode);
 
 	}
 
@@ -48,10 +91,13 @@ function onNodeCreated(astNode) {
 
 var parser = require('luaparse');
 const options = {
+	scope: true,
 	locations: true,
-	onCreateNode: onNodeCreated
+	onCreateNode: onNodeCreated,
+	onCreateFunctionDefinition: onFunctionDefinition
 }
 var ast = parser.parse(fileContents, options);
 // console.log(JSON.stringify(ast));
 // console.log("Namespace: %s", namespaceOrModuleName);
-console.log("Exported functions: %d\n", exportedApiSurface.length, exportedApiSurface);
+// console.log("Exported functions: %d\n", exportedApiSurface.length, exportedApiSurface);
+console.log(exportedApiSurface);
