3e2a9731f92aa3b975a28be851f4da5443a44679
RDW <duckwhale@mail.ru>
2021-11-05 15:28:45 +0100

    add ast

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let fs = require("fs");
+let fileContents = fs.readFileSync("C_FileSystem.lua").toString();
+// console.log(fileContents.toString());
+
 var parser = require('luaparse');
-var ast = parser.parse('i = 0');
+var ast = parser.parse(fileContents);
 console.log(JSON.stringify(ast));
