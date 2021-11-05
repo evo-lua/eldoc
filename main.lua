be0a9c7abde94c2c7d62785d768d8f87e38baaa8
RDW <duckwhale@mail.ru>
2021-11-05 15:23:47 +0100

    Create a blank slate for the luaparse experiment

--- main.lua
+++
@@ -1,3 +0,0 @@
--- This is only needed because evo-luvi assumes a bundle's entry point to be main.lua
--- It's possible to change this when running locally (-m switch), but it doesn't work when running a compiled bundle
-_G.import("ldoc")
